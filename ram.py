10c1c68333ad9c3ef9a7b891d0778e76c45dfe18
Pedro Bianchini de Quadros <pedrobiqua@gmail.com>
2023-08-22 21:49:10 -0300

    Documentação | Documentação dos metodos read e write

--- ram.py
+++ ram.py
@@ -6,9 +6,22 @@ class RAM(Memoria):
         self.memoria = [0] * self.tamanho()
 
     def read(self, ender):
+        """
+        Le os valores contidos na RAM.
+
+        :param ender: int
+
+        :return int Valor contido no endereço passado
+        """
         super().verifica_endereco(ender)
         return self.memoria[ender]
 
     def write(self, ender, val):
+        """
+        Escreve um valor em um endereço na RAM.
+
+        :param ender: int
+        :param val: int
+        """
         super().verifica_endereco(ender)
         self.memoria[ender] = val
